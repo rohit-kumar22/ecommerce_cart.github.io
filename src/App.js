845f9376f387482f007197d3fd8025ec85058994
Mohit kumar <83944002+rohit-kumar22@users.noreply.github.com>
2022-07-22 16:32:01 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { data } from "./ProductsData";
 
 function App() {
+  const cart = [];
+
+  function add(item) {
+    let found = cart.find((element) => element === item);
+    if (found !== undefined) {
+      console.log("Item is already present");
+    } else {
+      cart.push(item);
+      const totalItems = cart.length;
+
+      console.log(cart);
+      console.log("item added successfully");
+      console.log(`Total items in cart ${totalItems}`);
+    }
+  }
+
+  function remove(item) {
+    let found = cart.findIndex((element) => element === item);
+
+    if (found !== -1) {
+      cart.splice(found, 1);
+      const totalItems = cart.length;
+      console.log("item removed successfully");
+      console.log(`Total items in cart ${totalItems}`);
+    } else {
+      console.log("Item is not present in the cart");
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="nav">
+        <h2>Parakh</h2>
+      </div>
+      <div>
+        <h1>Our Products</h1>
+      </div>
+      {data.data.map((item, index) => (
+        <div className="product-bar">
+          <p style={{ border: "1px solid black" }}>{item.name}</p>
+          {item.productList.map((product) => (
+            <div style={{ paddingTop: "20px" }}>
+              <p>{product.name}</p>
+              <p>{product.price}</p>
+              <button
+                type="addToCart"
+                value="Add To Cart"
+                id="true"
+                className="button1"
+                onClick={() => add(`${product.name}`)}>
+                Add to the Cart
+              </button>
+              <button
+                type="removeFromCart"
+                value="Remove From Cart"
+                id="true"
+                className="button2"
+                onClick={() => remove(`${product.name}`)}>
+                Remove From Cart
+              </button>
+            </div>
+          ))}
+          <div></div>
+        </div>
+      ))}
     </div>
   );
 }
---
+++ src/ProductsData.js
@@ -0,0 +1,69 @@
+export const data = {
+  data: [
+    {
+      name: "Cosmetics",
+
+      productList: [
+        {
+          name: "Hair Oil",
+
+          price: 180,
+        },
+
+        {
+          name: "Face wash",
+
+          price: 110,
+        },
+
+        {
+          name: "Hair comb",
+
+          price: 99,
+        },
+
+        {
+          name: "Perfume",
+
+          price: 199,
+        },
+
+        {
+          name: "Nail polish",
+
+          price: 150,
+        },
+      ],
+    },
+
+    {
+      name: "Household",
+
+      productList: [
+        {
+          name: "Wooden Table",
+
+          price: 3500,
+        },
+
+        {
+          name: "Table fan",
+
+          price: 1500,
+        },
+
+        {
+          name: "Cooler",
+
+          price: 12000,
+        },
+
+        {
+          name: "LED",
+
+          price: 18000,
+        },
+      ],
+    },
+  ],
+};
